dfdd52d056285c51d5361f8390c3b8313fdbba24
aaronchen2007 <aaronchen20072007@gmail.com>
2024-05-30 09:04:29 +0800

    Learned about BITWISE operations

--- bitwise.py
+++ bitwise.py
@@ -9,7 +9,21 @@ circle = cv.circle(blank.copy(), (200,200), 200, 255, -1)
 cv.imshow('Rectangle', rectangle)
 cv.imshow('Circle', circle)
 
+# bitwise AND --> intersecting regions
+bitwise_and = cv.bitwise_and(rectangle,circle)
+cv.imshow('Bitwise AND', bitwise_and)
 
+# bitwise OR --> non-intersecting and intersecting regions
+bitwise_or = cv.bitwise_or(rectangle, circle)
+cv.imshow('Bitwise OR', bitwise_or)
+
+# bitwise XOR --> non-intersecting regions
+bitwise_xor = cv.bitwise_xor(rectangle, circle)
+cv.imshow('Bitwise XOR', bitwise_xor)
+
+# bitwise NOT
+bitwise_not = cv.bitwise_not(rectangle, circle)
+cv.imshow('Bitwise NOT', bitwise_not)
 
 
 cv.waitKey(0)
