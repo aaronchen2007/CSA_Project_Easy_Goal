4d9d3fc6418619b72793854e278727047fc72ed7
aaronchen2007 <aaronchen20072007@gmail.com>
2024-05-30 09:48:17 +0800

    Learned about thresholding

---
+++ thresh.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+img = cv.imread('/Users/aaronchen/Documents/GitHub/CSA_Project_Easy_Goal/Resources/Photos/cats.jpg')
+cv.imshow('Cats', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# Simple Thresholding
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+cv.imshow('Simple Thresholded', thresh)
+
+# Adaptive THresholding
+adpative_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 3)
+cv.imshow('Adaptive Thresholding', adpative_thresh)
+
+cv.waitKey(0)
