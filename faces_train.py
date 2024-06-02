2b8ee9e8b9707a6bf2bbd202bb05aa773b5294f2
aaronchen2007 <aaronchen20072007@gmail.com>
2024-06-02 12:23:56 +0800

    finally solved the face module issue encountered on Friday

--- faces_train.py
+++ faces_train.py
@@ -1,8 +1,46 @@
+import os
 import cv2
-print("OpenCV version:", cv2.__version__)
-print("Modules available:", dir(cv2))
+import numpy as np
 
-if hasattr(cv2, 'face'):
-    print("Face module is available")
-else:
-    print("Face module is NOT available")
+
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+DIR = r'/Users/aaronchen/Documents/GitHub/CSA_Project_Easy_Goal/Resources/Faces/train'
+
+haar_cascade = cv2.CascadeClassifier('haar_face.xml')
+
+features = []
+labels = []
+
+def create_train():
+    for person in people:
+        path = os.path.join(DIR, person)
+        label = people.index(person)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path, img)
+
+            img_array = cv2.imread(img_path)
+            gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
+            for (x, y, w, h) in faces_rect:
+                faces_roi = gray[y:y + h, x:x + w]
+                features.append(faces_roi)
+                labels.append(label)
+
+
+create_train()
+print('Training done ----------')
+
+features = np.array(features, dtype='object')
+labels = np.array(labels)
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+# Train the Recognizer on the features list and the labels list
+face_recognizer.train(features, labels)
+
+face_recognizer.save('face_trained.yml')
+np.save('features.npy', features)
+np.save('label.npy', labels)
